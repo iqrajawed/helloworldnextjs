cba30d728a37b6c57c12193a6d8e6159f7699c8b
Iqra Jawed <162354207+iqrajawed@users.noreply.github.com>
2024-12-21 14:21:24 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-
+import Link from "next/link";
+import react from "react";
 
 
 export default function Home() {
@@ -9,10 +9,13 @@ export default function Home() {
   
       <h1 className="flex justify-center hover:uppercase text-5xl pt-5  text-purple-600 font-serif ">Hello world</h1>
       
-        <p>To learn more about the folder structure click the button below</p>
+        <p className="flex justify-center text-yellow-300 pt-5">To learn more about the folder structure click the button below</p>
         
-        <button className=" py-2 px-5 mx-80 mt-4  text-yellow-300 hover:bg-purple-400 rounded-lg"> Learn more 
-          </button>
+        <div className="flex justify-center">
+          <Link href={"https://github.com/iqrajawed/helloworldnextjs.git"}>
+          <button  className=" py-2 px-5 mx-80 mt-4  text-yellow-300 hover:bg-purple-400 rounded-lg"> Learn more </button>
+          </Link>
+          </div>
         
      
     </div>
